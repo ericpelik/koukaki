8b2aabc0712db37c8a7b930f21a3ce62fb89a19c
ericpelik <pleyber.eric@gmail.com>
2024-05-24 21:37:33 +0200

    koukaki

--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -2,6 +2,8 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/sass/css/index.css' );
+
 }
 
 // Get customizer options form parent theme
@@ -14,3 +16,19 @@ if ( get_stylesheet() !== get_template() ) {
         return get_option( 'theme_mods_' . get_template(), $default );
     } );
 }
+
+function enqueue_swiper(){
+    wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css', array(), '10.3.1');
+    wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js', array('jquery'), '10.3.1', true);
+}
+
+add_action('wp_enqueue_scripts', 'enqueue_swiper');
+
+function loadScriptChild(){
+    wp_enqueue_script('swiper', get_stylesheet_directory_uri() . '/js/swiper.js', array('jquery'), '1.2.14', true);
+    wp_enqueue_script('cloud', get_stylesheet_directory_uri() . '/js/cloud.js', array('jquery'), '1.0.0', true);
+
+}
+
+add_action('wp_enqueue_scripts', 'loadScriptChild');
+?>
---
+++ wp-content/themes/foce-child/js/swiper.js
@@ -0,0 +1,20 @@
+console.log('bonjour')
+let swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
+  loop:true,
+  autoplay:{
+    delay:1000,
+
+  },
+});
